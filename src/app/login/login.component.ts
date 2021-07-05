ce18e97198bf6dc18213b9b2e9f5d4599897ca97
toncy <toncysara12@gmail.com>
2021-07-05 15:59:12 +0530

    login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  aim="Welcome to Luminar Bank";
+  account="Account Number Please";
+  accno="";
+  pswd="";
+  users={
+    1000:{acno:1000,username:"userone",password:"userone",actype:"savings",balance:5000},
+    1001:{acno:1001,username:"usertwo",password:"usertwo",actype:"savings",balance:6000},
+    1002:{acno:1002,username:"userthree",password:"userthree",actype:"savings",balance:7000},
+
+}
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  accChange(event:any){
+    this.accno=event.target.value;
+    console.log(this.accno);
+    
+  }
+pswdChange(event:any){
+  this.pswd=event.target.value;
+  console.log(this.pswd);
+  
+}
+
+
+  login(){
+    alert("Login Successful")
+  }
+
+}
